52b9ed09be2daca914317cf310a13afd2caed448
pallvi <pallavikamboj267@gmail.com>
2024-02-11 13:52:04 +0530

    Add length of last word solution

---
+++ array/length-of-last-word.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        reverse(s.begin(), s.end());
+
+        int index = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s[i] != ' ') {
+                index = i;
+                break;
+            }
+
+        }
+
+        int count = 0;
+
+        for(int i = index; i < s.length(); i++) {
+            cout<<s[i]<<endl;
+            if(s[i] == ' ') break;
+
+            count++;
+        }
+
+        return count;
+    }
+};
