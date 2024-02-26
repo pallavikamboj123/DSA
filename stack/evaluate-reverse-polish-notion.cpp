d921516093fdcbef50cb745edda19ac99ec69cc5
pallvi <pallavikamboj267@gmail.com>
2024-02-26 16:28:50 +0530

    Add sol for reverse polish notion

---
+++ stack/evaluate-reverse-polish-notion.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> s;
+
+        for(int i = 0; i < tokens.size(); i++) {
+            if(tokens[i] != "+" && tokens[i] != "-" && tokens[i] != "*" && tokens[i] != "/") {
+                s.push(stoi(tokens[i]));
+
+            }
+            else {
+                if(tokens[i] == "+") {
+                    int a = s.top();
+                    s.pop();
+                    int b = s.top();
+                    s.pop();
+                    s.push(a + b);
+                }
+
+                if(tokens[i] == "-") {
+                    int a = s.top();
+                    s.pop();
+                    int b = s.top();
+                    s.pop();
+                    s.push(b - a);
+                }
+
+                if(tokens[i] == "*") {
+                    int a = s.top();
+                    s.pop();
+                    int b = s.top();
+                    s.pop();
+                    s.push(a * b);
+                }
+
+                if(tokens[i] == "/") {
+                    int a = s.top();
+                    s.pop();
+                    int b = s.top();
+                    s.pop();
+                    s.push(b/a);
+                }
+            }
+
+
+        }
+
+        return s.top();
+    }
+};
