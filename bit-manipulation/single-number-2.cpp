1b6d46fa66d10ccd5b3bc5dbb03b6e9f29818521
pallvi <pallavikamboj267@gmail.com>
2024-03-18 15:23:33 +0530

    Add sol for single number 2

---
+++ bit-manipulation/single-number-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+
+        for(int i = 0; i < 32; i++) {
+            int sum = 0;
+            for(int j = 0; j < nums.size(); j++) {
+                sum += (nums[j] >> i) & 1;
+            }
+            sum = sum % 3;
+
+            ans = ans | sum << i;
+        }
+
+        return ans;
+    }
+};
