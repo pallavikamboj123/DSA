e2fd02ea2cd3123b6bae5d4d5d62e2383c6c4de4
pallvi <pallavikamboj267@gmail.com>
2024-04-24 17:56:32 +0530

    Add sol for find eventual safe states

---
+++ graph/find-eventual-safe-states.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+
+        vector<int> outDegree(n, 0), ans;
+
+        vector<vector<int>> adj(n);
+
+
+        for(int i = 0; i < n; i++) {
+            for(auto j : graph[i]) {
+                adj[j].push_back(i);
+            }
+        }
+
+        queue<int> q;
+        for(int i = 0; i < n; i++) {
+            outDegree[i] += graph[i].size();
+            if(!outDegree[i]) q.push(i);
+        }
+
+        while(!q.empty()) {
+            int front = q.front();
+            q.pop();
+
+            ans.push_back(front);
+
+            for(auto nbr: adj[front]) {
+                if(--outDegree[nbr] == 0) {
+                    q.push(nbr);
+                }
+            }
+        }
+
+        sort(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
