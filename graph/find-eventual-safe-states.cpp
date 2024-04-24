3babbb0a5d26e720fa0167f5e3e5ebe799416f1e
pallvi <pallavikamboj267@gmail.com>
2024-04-24 18:07:26 +0530

    Add in degree sol for find eventual safe nodes

--- graph/find-eventual-safe-states.cpp
+++ graph/find-eventual-safe-states.cpp
@@ -38,3 +38,47 @@ public:
         return ans;
     }
 };
+
+
+//In degree solution
+
+class Solution {
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+
+        vector<int> inDegree(n, 0), ans;
+
+        vector<vector<int>> adj(n);
+
+
+        for(int i = 0; i < n; i++) {
+            for(auto j : graph[i]) {
+                adj[j].push_back(i);
+                inDegree[i]++;
+            }
+        }
+
+        queue<int> q;
+        for(int i = 0; i < n; i++) {
+            if(!inDegree[i]) q.push(i);
+        }
+
+        while(!q.empty()) {
+            int front = q.front();
+            q.pop();
+
+            ans.push_back(front);
+
+            for(auto nbr: adj[front]) {
+                if(--inDegree[nbr] == 0) {
+                    q.push(nbr);
+                }
+            }
+        }
+
+        sort(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
