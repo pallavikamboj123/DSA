11d7848d090f1293461d25c1a1ba9c45217ff9f3
pallvi <pallavikamboj267@gmail.com>
2024-04-28 18:27:26 +0530

    Add iterative dp sol for triangle

--- 2d-dp/triangle.cpp
+++ 2d-dp/triangle.cpp
@@ -57,3 +57,21 @@ public:
         return dfs(triangle, 0, 0, memo);
     }
 };
+
+#iterative dp
+
+class Solution {
+public:
+
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+
+        for(int i = n -2; i >= 0; i--) {
+            for(int j = 0; j < triangle[i].size(); j++) {
+                triangle[i][j] = min(triangle[i + 1][j], triangle[i+1][j+1]) + triangle[i][j];
+            }
+        }
+
+        return triangle[0][0];
+    }
+};
