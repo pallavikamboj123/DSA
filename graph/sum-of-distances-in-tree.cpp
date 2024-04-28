a46ec0fcd4ffb583833d64656bba38a5e980f073
pallvi <pallavikamboj267@gmail.com>
2024-04-28 16:13:14 +0530

    Add optimised sol for sum of distances in tree

--- graph/sum-of-distances-in-tree.cpp
+++ graph/sum-of-distances-in-tree.cpp
@@ -44,3 +44,70 @@ public:
         return dist;
     }
 };
+
+
+# dfs solution where use parent node to find the ans (ans = ans of parent - no of nodes of child including child + remaning nodes)
+
+class Solution {
+public:
+    int totalSum = 0;
+    int dfs(vector<vector<int>>& adj, int node, int level, vector<bool>& visited, vector<int>& child) {
+        visited[node] = true;
+
+        totalSum += level;
+        int ch = 0;
+
+        for(auto nbr : adj[node]) {
+            if(!visited[nbr]) {
+                ch += dfs(adj, nbr, level + 1, visited, child );
+            }
+        }
+
+        child[node] = ch + 1;
+
+        return ch + 1;
+    }
+
+    void dfs2(vector<vector<int>>& adj, int node, int parent, vector<int>& dist, vector<bool>& visited, vector<int>& child, int n) {
+        visited[node] = true;
+        cout<<node<<" "<<parent<<endl;
+
+        for(auto nbr: adj[node]) {
+            if(!visited[nbr]) {
+
+                dist[nbr] = dist[node] - child[nbr] +  (n - child[nbr]);
+
+
+                dfs2(adj, nbr, node, dist, visited, child, n);
+            }
+        }
+    }
+
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        vector<int> dist(n);
+        vector<int> child(n, 0);
+        vector<vector<int>> adj(n);
+
+        for(auto edge: edges) {
+            int node1 = edge[0];
+            int node2 = edge[1];
+
+            adj[node1].push_back(node2);
+            adj[node2].push_back(node1);
+        }
+
+        vector<bool> visited(n, false);
+
+
+        child[0] = dfs(adj, 0, 0, visited, child);
+
+        dist[0] = totalSum;
+
+        for(int i = 0; i < n; i++) visited[i] = false;
+
+        dfs2(adj, 0, -1, dist, visited, child, n);
+
+        return dist;
+    }
+};
+
