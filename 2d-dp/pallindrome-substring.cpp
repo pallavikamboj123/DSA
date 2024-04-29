fe6bc17a3b4d134e4f9279753915113fddc0116d
pallvi <pallavikamboj267@gmail.com>
2024-04-29 17:52:04 +0530

    Add sol for pallindrome substring with iterative dp

--- 2d-dp/pallindrome-substring.cpp
+++ 2d-dp/pallindrome-substring.cpp
@@ -66,3 +66,49 @@ public:
         return ans;
     }
 };
+
+#iterative dp(blueprint method)
+
+class Solution {
+public:
+
+    int countSubstrings(string s) {
+        int n = s.length();
+        int ans = 0;
+        vector<vector<bool>> dp(n);
+
+        for(int i = 0 ; i < n; i++) {
+            vector<bool> v;
+            for(int j = 0; j < n; j++) v.push_back(false);
+
+            dp.push_back(v);
+        }
+
+        for(int i = 0; i < n; i++) dp[i][i] = true;
+
+        int i = 0;
+
+        while(i < n - 1) {
+            int a = 0, b = i + 1;
+
+            while(b < n) {
+                if(a + 1 < b - 1) {
+                    dp[a][b] = (s[a] == s[b]) && dp[a+1][b-1];
+                }
+                else {
+                    dp[a][b] = (s[a] == s[b]);
+                }
+
+                if(dp[a][b]) {
+                    ans++;
+                }
+
+                a++;
+                b++;
+            }
+            i++;
+        }
+
+        return ans + s.length();
+    }
+};
