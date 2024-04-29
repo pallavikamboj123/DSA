527a54f1aeea940312bd1c23cd759e88aaff7e7c
pallvi <pallavikamboj267@gmail.com>
2024-04-29 17:07:23 +0530

    Add recursion and memo sol for no of pallindrome

---
+++ 2d-dp/pallindrome-substring.cpp
@@ -0,0 +1,68 @@
+#tle
+class Solution {
+public:
+    bool countPallin(string s, int i, int j) {
+        if(i > j) return true;
+
+        if(s[i] != s[j]) return false;
+
+        if(countPallin(s, i + 1, j - 1)) return true;
+
+        return false;
+    }
+    int countSubstrings(string s) {
+        int n = s.length();
+        int ans = 0;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                if(countPallin(s, i, j)) ans++;
+            }
+        }
+
+        return ans;
+    }
+};
+
+#memo
+
+class Solution {
+public:
+    bool countPallin(string s, int i, int j, vector<vector<int>>& memo) {
+        if(i > j) return true;
+
+        if(memo[i][j] != -1) return memo[i][j];
+
+        if(s[i] != s[j]) return false;
+
+        if(countPallin(s, i + 1, j - 1, memo)) {
+            memo[i][j] = true;
+            return true;
+
+        }
+
+        memo[i][j] = false;
+
+        return false;
+    }
+    int countSubstrings(string s) {
+        int n = s.length();
+        int ans = 0;
+        vector<vector<int>> memo;
+
+        for(int i = 0 ; i < n; i++) {
+            vector<int> v;
+            for(int j = 0; j < n; j++) v.push_back(-1);
+
+            memo.push_back(v);
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                if(countPallin(s, i, j, memo)) ans++;
+            }
+        }
+
+        return ans;
+    }
+};
