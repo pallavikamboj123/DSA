1ab854db7fcb80e09e85d4edf37f90f59fe975c4
pallvi <pallavikamboj267@gmail.com>
2024-05-02 10:51:19 +0530

    Add sorting sol for  Largest Positive Integer That Exists With Its Negative

---
+++ array/largest-positive-integer-that-exists-with-its-array.cpp
@@ -0,0 +1,18 @@
+#using sorting 
+class Solution {
+public:
+    int findMaxK(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int i = 0, j = nums.size() - 1;
+
+        while(i < j) {
+            if(nums[i] + nums[j] == 0) return nums[j];
+
+            if(nums[i] +nums[j] > 0) j--;
+            else i++;
+        }
+
+        return -1;
+    }
+};
