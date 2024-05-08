14ae19c49b7ecc39c94bc326b3e2fcbd5c2db46e
pallvi <pallavikamboj267@gmail.com>
2024-05-08 18:15:49 +0530

    Add optimal solution for moving zeroes

--- array/move-zeroes.cpp
+++ array/move-zeroes.cpp
@@ -46,3 +46,20 @@ public:
         }
     }
 };
+
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+
+        int i = 0, j = 0;
+        while(i < n && j < n) {
+            if(nums[j] != 0) {
+                swap(nums[i], nums[j]);
+                i++;
+            }
+            j++;
+        }
+    }
+};
