b2d1986743106c8ce853d446b5eb12b512950fa1
pallvi <pallavikamboj267@gmail.com>
2024-05-08 18:12:56 +0530

    Add another sol for moving zeroes

--- array/move-zeroes.cpp
+++ array/move-zeroes.cpp
@@ -22,3 +22,27 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+
+        int i = 0, j = 0;
+        while(i < n && j < n) {
+            while(i < n && nums[i] != 0) {
+                i++;
+            }
+
+            j = i + 1;
+            while(j < n && nums[j] == 0) j++;
+
+            if(i >= n || j >= n) return;
+
+            nums[i] = nums[j];
+            nums[j] = 0;
+            i++;
+            j++;
+        }
+    }
+};
