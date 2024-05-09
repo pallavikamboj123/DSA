c172373be101fca79c73f6336275979cfe14a741
pallvi <pallavikamboj267@gmail.com>
2024-05-09 17:04:35 +0530

    Add sol for remove duplicates from sorted array

--- array/remove-duplicates-from-sorted-array.cpp
+++ array/remove-duplicates-from-sorted-array.cpp
@@ -17,3 +17,18 @@ public:
         return s.size();
     }
 };
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int i = 0;
+        for(int j = 1; j < nums.size(); j++) {
+            if(nums[j] != nums[i]) {
+                nums[i+1] = nums[j];
+                i++;
+            }
+        }
+
+        return i + 1;
+    }
+};
