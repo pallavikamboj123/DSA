35d391449f5a3be6aa41a1cb616d82e7d011f66c
pallvi <pallavikamboj267@gmail.com>
2024-05-31 14:46:25 +0530

    Remove unneccesary main function

--- array/longest-subarray-with-sum-k.cpp
+++ array/longest-subarray-with-sum-k.cpp
@@ -32,26 +32,3 @@ class Solution{
     }
 
 };
-
-//{ Driver Code Starts.
-
-int main() {
-	//code
-
-	int t;cin>>t;
-	while(t--)
-	{
-	    int n, k;
-	    cin>> n >> k;
-	    int a[n];
-
-	    for(int i=0;i<n;i++)
-	        cin>>a[i];
-	   Solution ob;
-	   cout << ob.lenOfLongSubarr(a, n , k)<< endl;
-
-	}
-
-	return 0;
-}
-// } Driver Code Ends
